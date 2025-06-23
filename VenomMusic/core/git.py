f48f1b1cdf9e4ff64d64d336863d62009fb6844f
Venom Pratap <venommusic91@gmail.com>
2025-06-23 16:52:46 +0530

    Update git.py

--- VenomMusic/core/git.py
+++ VenomMusic/core/git.py
@@ -1,6 +1,33 @@
-from git import Repo, InvalidGitRepositoryError
-import os
+import asyncio
+import shlex
+from typing import Tuple
+
+from git import Repo
+from git.exc import GitCommandError, InvalidGitRepositoryError
+
 import config
+
+from ..logging import LOGGER
+
+
+def install_req(cmd: str) -> Tuple[str, str, int, int]:
+    async def install_requirements():
+        args = shlex.split(cmd)
+        process = await asyncio.create_subprocess_exec(
+            *args,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        stdout, stderr = await process.communicate()
+        return (
+            stdout.decode("utf-8", "replace").strip(),
+            stderr.decode("utf-8", "replace").strip(),
+            process.returncode,
+            process.pid,
+        )
+
+    return asyncio.get_event_loop().run_until_complete(install_requirements())
+
 
 def git():
     REPO_LINK = config.UPSTREAM_REPO
